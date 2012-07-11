43e29e2eab156be572f879ec2c0d3c3e3faef215
josephboccuzzi <joseph@designerpages.com>
2012-07-11 12:17:52 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LeadReport::Application.config.secret_token = '7184b4c6163f95ceac83c74c8d2874d3a1c59dc726f9ab4a7b2c0e258278b088acde430db3180cd5818985659ff842d21e31766bc4af9470e39c78d9e1209909'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+LeadReport::Application.config.session_store :cookie_store, :key => '_lead_report_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# LeadReport::Application.config.session_store :active_record_store
